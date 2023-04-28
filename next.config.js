071fadabab9d5699f1f05b32e294ab94fe2fd4d5
Dillion Verma <hello@dillionverma.com>
2023-04-28 02:16:20 -0400

    🐛 fix: remove logs from prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   },
