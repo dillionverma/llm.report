8e064cf6dd33c6fa68cd57444619d1db916dd283
Dillion Verma <hello@dillionverma.com>
2023-04-29 08:47:36 -0400

    🐛 fix: add subscribe badge

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -125,6 +125,21 @@ export default function KpiCardGrid() {
               </Badge>
             )}
 
+            {key && !subscribed && (
+              <Badge
+                className="px-3 space-x-2"
+                color="red"
+                icon={() => (
+                  <span className="relative flex h-3 w-3">
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+                  </span>
+                )}
+              >
+                subscribe to a plan
+              </Badge>
+            )}
+
             {key && subscribed && (
               <Badge
                 className="px-3 space-x-2"
