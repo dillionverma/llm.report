9769d5515c46a263a0f0fe4e427d167b9a0613f2
Dillion Verma <hello@dillionverma.com>
2023-04-29 19:34:01 -0400

    📈 analytics: Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 
 export default function App({
@@ -12,6 +13,21 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-YWRE5VL6F8"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YWRE5VL6F8');
+          `}
+      </Script>
       <DialogProvider>
         <Meta />
         <Component {...pageProps} />
