335fd7323e17e7dfeb528cc0227bdd0449199795
Naveen <mnaveennaidu9927@gmail.com>
2023-08-03 11:29:27 +0530

    saving the logs to tinybird

--- pages/api/v1/log/openai.ts
+++ pages/api/v1/log/openai.ts
@@ -6,8 +6,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    console.log("POST /api/v1/log/openai", req.body);
-
     const llmApikey = getLlmReportApiKey(req);
     if (!llmApikey) {
       return res.status(401).json({
@@ -23,34 +21,19 @@ export default async function handler(
         error: "User not found.",
       });
     }
-    prisma.request.create({
-      data: {
-        openai_id: req.body.id,
-        metadata: req.body.metadata,
 
-        ip: "",
-
-        url: req.body.url,
-        method: req.body.method,
-        status: req.body.status,
-        cached: req.body.cached,
-        streamed: req.body.streamed,
-        user_id: req.body.user_id,
-
-        model: req.body.model,
-        prompt_tokens: req.body.prompt_tokens,
-        completion_tokens: req.body.completion_tokens,
-
-        request_headers: req.body.request_headers,
-        request_body: req.body.request_body,
-
-        response_headers: req.body.response_headers,
-        response_body: req.body.response_body,
-        streamed_response_body: req.body.streamed_response_body,
-
-        completion: req.body.completion,
-      },
-    });
+    try {
+      const body = { ...req.body, llm_report_user_id: user.id };
+      await fetch("https://api.us-east.tinybird.co/v0/events?name=openai_log", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}`,
+        },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
     return res.status(200).json({ success: true });
   } else {
     return res.status(405).json({ error: "Method not allowed" });
